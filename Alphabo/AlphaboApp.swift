dc3af148ed5d6fc2939777b4327a818e9c5c84ee
Ben Palmer <benj.c.palmer@gmail.com>
2022-12-06 15:20:07 -0500

    Initial Commit

---
+++ Alphabo/AlphaboApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaboApp.swift
+//  Alphabo
+//
+//  Created by Benjamin Palmer on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AlphaboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
